be9d319ad1e2ce78a1bf35b85a582111577b8a8b
Dark Dante <92054470+0DarkDante@users.noreply.github.com>
2022-10-31 10:57:30 +0200

    Дороблена сторінка католог, початок сторінки NikeTiempo

--- catalog.js
+++ catalog.js
@@ -7,3 +7,14 @@ price.addEventListener('input', () => {
   priceOut.textContent=price.value;
 });
 
+// pagination
+const pagin = document.getElementsByClassName("pagination"),
+      BtnPagin = document.getElementsByClassName("btn_pagin");
+
+      for (let i = 0; i < BtnPagin.length; i++) {
+        BtnPagin[i].addEventListener("click", function() {
+          let current = document.getElementsByClassName("active");
+          current[0].className = current[0].className.replace(" active", "");
+          this.className += " active";
+        });
+      }
